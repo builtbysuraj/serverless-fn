e83ac04cea71d8b617f3bb17e380429a23fd95bb
builtbysuraj <surajgupta3553@gmail.com>
2025-01-29 15:22:37 +0530

    Add post endpoint

--- api/index.js
+++ api/index.js
@@ -5,7 +5,12 @@ const app = express()
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express on Vercel!')
+  return res.json({ message: 'Hello' })
+})
+
+app.post('/post', (req, res) => {
+  const data = req.body
+  return res.json({ data: data })
 })
 
 export default app
