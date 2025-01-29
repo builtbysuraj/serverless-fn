2024c10ed4b4963503987191658ded454aa2603f
builtbysuraj <surajgupta3553@gmail.com>
2025-01-29 15:26:47 +0530

    Move main code to server.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,3 @@
-import cors from 'cors'
-import express from 'express'
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello' })
-})
-
-app.post('/post', (req, res) => {
-  const data = req.body
-  return res.json({ data: data })
-})
+import app from '../server.js'
 
 export default app
--- server.js
+++ server.js
@@ -31,3 +31,21 @@
 // })
 
 // module.exports = app
+
+import cors from 'cors'
+import express from 'express'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello' })
+})
+
+app.post('/post', (req, res) => {
+  const data = req.body
+  return res.json({ data })
+})
+
+export default app
